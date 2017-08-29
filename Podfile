a9e0d114f3767aaf6220f18161359ff82165cf6a
jimrain <jim@rainville.net>
2017-08-29 06:56:15 -0700

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/brightcove/BrightcoveSpecs.git'
+source 'https://github.com/CocoaPods/Specs.git'
+
+use_frameworks!
+
+platform :ios, '10.0' # (or whatever version you want)
+
+target 'VCPlaylistIOS' do
+  pod 'Brightcove-Player-IMA'
+end
---
+++ VCPlaylistIOS/SVP-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SVP-Bridging-Header.h
+//  VCPlaylistIOS
+//
+//  Created by James Rainville on 8/29/17.
+//  Copyright © 2017 James Rainville. All rights reserved.
+//
+
+#ifndef SVP_Bridging_Header_h
+#define SVP_Bridging_Header_h
+
+#import <BrightcovePlayerSDK/BrightcovePlayerSDK.h>
+
+#endif /* SVP_Bridging_Header_h */
