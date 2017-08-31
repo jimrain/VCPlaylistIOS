87b60cca155b2c9ed2aef84075e383e6d4b29b81
jimrain <jim@rainville.net>
2017-08-30 20:22:06 -0700

    Add video quick start

--- VCPlaylistIOS/ViewController.swift
+++ VCPlaylistIOS/ViewController.swift
@@ -5,14 +5,62 @@
 //  Created by James Rainville on 8/28/17.
 //  Copyright © 2017 James Rainville. All rights reserved.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
+let kViewControllerPlaybackServicePolicyKey = "BCpkADawqM3dCmAPU7-jl0hHWW8097dehsjhdHYeCZVO3HbClNSwbtBpZkhuDuab141BnGkFL_xvPCif9v6Sz5A27pFUo8-qFuq42J6vzrXnLkmeLzGkQ1HzNdow2rI0qmhyPaEEhSGTPpW9"
+let kViewControllerAccountID = "4517911906001"
+let kViewControllerVideoID = "5269117590001"
+let kViewControllerPlaylistID = "5280100152001"
 
+
+class ViewController: UIViewController, BCOVPlaybackControllerDelegate, BCOVPUIPlayerViewDelegate {
+    let playbackService = BCOVPlaybackService(accountId: kViewControllerAccountID, policyKey: kViewControllerPlaybackServicePolicyKey)!
+    let playbackController :BCOVPlaybackController
+    @IBOutlet weak var videoContainer: UIView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        let manager = BCOVPlayerSDKManager.shared()!
+        playbackController = manager.createPlaybackController()
+        
+        super.init(coder: aDecoder)
+        
+        playbackController.analytics.account = kViewControllerAccountID
+        
+        playbackController.delegate = self
+        playbackController.isAutoAdvance = true
+        playbackController.isAutoPlay = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // Create and set options
+        let options = BCOVPUIPlayerViewOptions()
+        options.presentingViewController = self
+        
+        // Create and configure Control View
+        let controlsView = BCOVPUIBasicControlView.withVODLayout()
+        let playerView = BCOVPUIPlayerView.init(playbackController: playbackController, options: options, controlsView: controlsView)!
+        
+        playerView.delegate = self
+        playerView.frame = videoContainer.bounds
+        playerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        videoContainer.addSubview(playerView)
+        
+        requestContentFromPlaybackService()
+    }
+    
+    func requestContentFromPlaybackService() {
+        playbackService.findVideo(withVideoID: kViewControllerVideoID, parameters: nil) {
+            (video: BCOVVideo?, dict: [AnyHashable:Any]?, error: Error?) in
+            if let v = video {
+                self.playbackController.setVideos([v] as NSFastEnumeration!)
+            } else {
+                print("ViewController Debug - Error retrieving video: %@", error!)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
