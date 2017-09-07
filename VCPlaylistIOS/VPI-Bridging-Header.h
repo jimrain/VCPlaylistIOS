b5dd444b6721d670d9147549d32ba8bf5e53dd59
jimrain <jim@rainville.net>
2017-09-07 05:32:22 -0700

    Adding all the IMA Libraries

---
+++ VCPlaylistIOS/VPI-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  SVP-Bridging-Header.h
+//  VCPlaylistIOS
+//
+//  Created by James Rainville on 8/29/17.
+//  Copyright © 2017 James Rainville. All rights reserved.
+//
+
+#ifndef VPI_Bridging_Header_h
+#define VPI_Bridging_Header_h
+
+#import <BrightcoveIMA/BrightcoveIMA.h>
+#import <BrightcovePlayerSDK/BrightcovePlayerSDK.h>
+@import GoogleInteractiveMediaAds;
+
+#endif /* SVP_Bridging_Header_h */
--- VCPlaylistIOS/ViewController.swift
+++ VCPlaylistIOS/ViewController.swift
@@ -12,16 +12,30 @@ let kViewControllerAccountID = "4517911906001"
 let kViewControllerVideoID = "5269117590001"
 let kViewControllerPlaylistID = "5280100152001"
 
+let kViewControllerIMAPublisherID = "ca-pub-4202951716165137"
+let kViewControllerIMALanguage = "en"
+let kViewControllerIMAVMAPResponseAdTag = "https://pubads.g.doubleclick.net/gampad/ads?sz=320x240&iu=/6390/BabyCenter&cust_params={mediainfo.ad_keys}&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url={document.referrer}&description_url={player.url}&correlator={timestamp}"
 
-class ViewController: UIViewController, BCOVPlaybackControllerDelegate, BCOVPUIPlayerViewDelegate {
+class ViewController: UIViewController, BCOVPlaybackControllerDelegate, BCOVPUIPlayerViewDelegate, IMAWebOpenerDelegate {
     let playbackService = BCOVPlaybackService(accountId: kViewControllerAccountID, policyKey: kViewControllerPlaybackServicePolicyKey)!
     let playbackController :BCOVPlaybackController
     @IBOutlet weak var videoContainer: UIView!
     
     required init?(coder aDecoder: NSCoder) {
-        let manager = BCOVPlayerSDKManager.shared()!
-        playbackController = manager.createPlaybackController()
         
+        let manager = BCOVPlayerSDKManager.shared()!
+        
+        let imaSettings = IMASettings()
+        imaSettings.ppid = kViewControllerIMAPublisherID
+        imaSettings.language = kViewControllerIMALanguage
+        let renderSettings = IMAAdsRenderingSettings()
+        renderSettings.webOpenerPresentingController = self
+        renderSettings.webOpenerDelegate = self as IMAWebOpenerDelegate
+        let adsRequestPolicy = BCOVIMAAdsRequestPolicy(vmapAdTagUrl: kViewControllerIMAVMAPResponseAdTag)
+
+        playbackController = manager.createIMAPlaybackController(with: imaSettings, adsRenderingSettings: renderSettings, adsRequestPolicy: adsRequestPolicy, adContainer: videoContainer, companionSlots: nil, viewStrategy: nil)
+        
+        // playbackController = manager.createPlaybackController()
         super.init(coder: aDecoder)
         
         playbackController.analytics.account = kViewControllerAccountID
